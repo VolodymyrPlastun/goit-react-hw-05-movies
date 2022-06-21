86bb616f4cee9ef0812d0d20c13f7c43ad6666a5
VolodymyrPlastun <vplastun8@gmail.com>
2022-06-21 18:16:03 +0300

    add

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <BrowserRouter basename="/goit-react-hw-05-movies/"> */}
-      {/* <div>Test</div> */}
-      <App />
+      <div>Test</div>
+      {/* <App /> */}
     {/* </BrowserRouter> */}
   </React.StrictMode>
 );
